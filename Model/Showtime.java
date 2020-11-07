9c28a857184128f8dd6f0e8d26333c40c947ccd0
Long Đặng <18127011@student.hcmus.edu.vn>
2020-11-07 21:08:21 +0700

    Add check_available_seat to Showtime class

--- Model/Showtime.java
+++ Model/Showtime.java
@@ -2,10 +2,12 @@ package Model;
 
 public class Showtime {
     Film film;
+    String startTime;
     boolean[][] seats; // Nếu là false có nghĩa là còn chỗ trống
 
-    public Showtime( Film film,int row,int col) {
+    public Showtime( Film film,String startTime,int row,int col) {
         this.film=film;
+        this.startTime=startTime;
         seats = new boolean[row][col];
         for(int i=0;i<seats.length;i++) {
             for(int j=0;j<col;j++) {
@@ -41,7 +43,17 @@ public class Showtime {
                 return false;
         }
     }
-
+    public String get_startTime(){
+        return startTime;
+    }
+    public String get_title(){
+        return film.get_title();
+    }
+    public boolean check_available_seat(int row, int col) {
+        if(row>seats.length ||col>seats[0].length ||row<0 ||col<0)
+            return false;
+        return seats[row][col];
+    }
 
     
 
