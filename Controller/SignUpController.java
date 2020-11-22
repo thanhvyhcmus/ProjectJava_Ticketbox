45a7b3ff97cfb38ebf0aeaded1bbeba0561c375b
Long Đặng <18127011@student.hcmus.edu.vn>
2020-11-22 13:31:30 +0700

    Update SignUpController

--- Controller/SignUpController.java
+++ Controller/SignUpController.java
@@ -5,22 +5,8 @@ import DAO.UserDAO;
 
 public class SignUpController {
     public static int addAnAccount(Customer account) {
-        char[] s = account.getUsername().toCharArray();
-        if (s.length ==0)
-            return 0;
-        for (char c : s) {
-            if (!(c >= 32 && c <= 126)) {
-                return 0;
-            }
-        }
-        s = account.getPassword().toCharArray();
-        if(s.length ==0)
-            return 0;
-        for (char c : s) {
-            if (!(c >= 32 && c <= 126)) {
-                return 0;
-            }
-        }
+        if(account.getID().length()!=0 && account.getDoB().length()!=0 && account.getPassword().length()!=0 && account.getUsername().length()!=0 && account.getFullname().length()!=0 && account.getPhone().length()!=0) 
+            return -1;
         if(UserDAO.checkAnAccount(account.getUsername(), account.getPassword())==1 || UserDAO.checkAnAccount(account.getUsername(), account.getPassword())==2 )
             return 0;
         if(UserDAO.addACustomer(account))
