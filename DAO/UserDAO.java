b0753bd482815a6ae629eabc7660433e4339c109
longd <18127011@student.hcmus.edu.vn>
2020-12-24 23:30:03 +0700

    Add hashPassword

--- DAO/UserDAO.java
+++ DAO/UserDAO.java
@@ -9,6 +9,11 @@ import java.util.logging.Logger;
 import Model.Admin;
 import Model.Customer;
 import Model.User;
+
+import java.math.BigInteger;  
+import java.nio.charset.StandardCharsets; 
+import java.security.MessageDigest;  
+import java.security.NoSuchAlgorithmException; 
 public class UserDAO {
     public static  ArrayList<Customer> getAllCustormer(){
         ArrayList<Customer> customer = new ArrayList<Customer>();
@@ -495,7 +500,37 @@ public class UserDAO {
         }    
         return user;   
     }    
-
+    public static byte[] getSHA(String input) throws NoSuchAlgorithmException 
+    {  
+        // Static getInstance method is called with hashing SHA  
+        MessageDigest md = MessageDigest.getInstance("SHA-256");  
+  
+        // digest() method called  
+        // to calculate message digest of an input  
+        // and return array of byte 
+        return md.digest(input.getBytes(StandardCharsets.UTF_8));  
+    } 
+    public static String toHexString(byte[] hash) 
+    { 
+        // Convert byte array into signum representation  
+        BigInteger number = new BigInteger(1, hash);  
+  
+        // Convert message digest into hex value  
+        StringBuilder hexString = new StringBuilder(number.toString(16));  
+  
+        // Pad with leading zeros 
+        while (hexString.length() < 32)  
+        {  
+            hexString.insert(0, '0');  
+        }  
+  
+        return hexString.toString();  
+    }
+    public static String hashPassword(String password) throws NoSuchAlgorithmException
+    {
+        return toHexString(getSHA(password));
+    }
 }
-    
+ 
+
 
