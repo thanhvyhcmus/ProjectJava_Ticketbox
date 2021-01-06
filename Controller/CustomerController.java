3a12266d01660a1383e78dde190b8d40af28c9d1
longd <18127011@student.hcmus.edu.vn>
2021-01-06 16:15:03 +0700

    Edit getallshowtimes in customer controller

--- Controller/CustomerController.java
+++ Controller/CustomerController.java
@@ -1,43 +1,57 @@
 package Controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import Model.*;
 
 public class CustomerController {
     Customer customer;
+
     public CustomerController(Customer customer) {
         this.customer = customer;
     }
-    public boolean bookTicket(Showtime showtime,int row,int col) {
-        Seat seat = new Seat(showtime.getID(),row,col,customer.getID(),0);
+
+    public boolean bookTicket(Showtime showtime, int row, int col) {
+        Seat seat = new Seat(showtime.getID(), row, col, customer.getID(), 0);
         ArrayList<Seat> seats = new ArrayList<Seat>();
         seats.add(seat);
         return DAO.ShowtimeDAO.bookTickets(seats);
     }
+
     public boolean bookTicket(ArrayList<Seat> seats) {
         return DAO.ShowtimeDAO.bookTickets(seats);
     }
-    public static ArrayList<Film> getAllFilms(){
+
+    public static ArrayList<Film> getAllFilms() {
         return DAO.FilmDAO.getAllFilm();
     }
+
     public static ArrayList<Showtime> getAllShowtimesBy(Film film) {
         return DAO.ShowtimeDAO.getAllShowtimesBy(film);
     }
-    public static ArrayList<Showtime> getAllShowtimesBy(Theater theater){
+
+    public static ArrayList<Showtime> getAllShowtimesBy(Theater theater) {
         return DAO.ShowtimeDAO.getAllShowtimesBy(theater);
     }
-    public static ArrayList<Showtime> getAllShowtimesBy(String theater,String film){
-//        ArrayList<Showtime> showtimes = new ArrayList<>();
-//        showtimes.add(new Showtime(1,new Theater(1,"",""),new Film(1,"","",1,"","","","","",""),"14:00"));
-//        showtimes.add(new Showtime(1,new Theater(1,"",""),new Film(1,"","",1,"","","","","",""),"14:00"));
-//        showtimes.add(new Showtime(1,new Theater(1,"",""),new Film(1,"","",1,"","","","","",""),"14:00"));
-//        showtimes.add(new Showtime(1,new Theater(1,"",""),new Film(1,"","",1,"","","","","",""),"14:00"));
-//        return showtimes;
-        return DAO.ShowtimeDAO.getAllShowtimesBy(theater,film);
+
+    public static ArrayList<Showtime> getAllShowtimesBy(int theater, int film) {
+        return DAO.ShowtimeDAO.getAllShowtimesBy(theater, film);
     }
-    public static ArrayList<Theater> getAllTheaters(){
+
+    public static ArrayList<Theater> getAllTheaters() {
         return DAO.TheaterDAO.getAllTheaters();
+    }
+
+    public static HashMap<String, ArrayList<Showtime>> getAllShowtimesByFilm(int film, String date) {
+        return DAO.ShowtimeDAO.getAllShowtimeByFilm(film, date);
+    }
+
+    public static HashMap<String, ArrayList<Showtime>> getAllShowtimesByTheater(int theater, String date) {
+        return DAO.ShowtimeDAO.getAllShowtimeByTheater(theater,date);
+    }
+    public static ArrayList<Showtime> getAllShowtimesBy(int theater,int film,String date){
+        return DAO.ShowtimeDAO.getAllShowtimeBy(theater,film,date);
     }
 
 }
